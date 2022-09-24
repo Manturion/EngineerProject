96aaf9cc40774ad8fe5e2e2adc0f233e3fdd18dd
przemoai <again364@gmail.com>
2022-09-24 14:59:09 +0200

    Code refactoring, added case when we can't load offers

--- frontend_android/app/src/main/java/pl/pollub/harnasik/app/presentation/allOffers/components/Offers.kt
+++ frontend_android/app/src/main/java/pl/pollub/harnasik/app/presentation/allOffers/components/Offers.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -18,13 +16,11 @@ import pl.pollub.harnasik.R
 import pl.pollub.harnasik.app.data.remote.Offer.OfferService
 import pl.pollub.harnasik.app.data.remote.Offer.dto.OfferResponse
 
-
 @Composable
 fun CategorySlideBar(categories: List<String> = List(10) { "Category $it" }) {
     LazyRow() {
         items(items = categories) { category ->
             SingleCategoryButton(name = category)
-
         }
     }
 }
@@ -46,7 +42,7 @@ fun getAllOffers() {
     val offers = produceState<List<OfferResponse>>(
         initialValue = emptyList(),
         producer = {
-            value = service.getOffers()
+            value = service.getAllOffers()
         })
 
     GenerateListOfAllOffersLoaded(offers)
@@ -54,16 +50,28 @@ fun getAllOffers() {
 
 @Composable
 fun GenerateListOfAllOffersLoaded(offers: State<List<OfferResponse>>) {
+
+    if (offers.value.isEmpty()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Nothing to show or connection refused",
+                modifier = Modifier,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
+
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
 
         items(offers.value) {
-
             val expanded = remember { mutableStateOf(false) }
 
             Surface(
                 onClick = {},
                 modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-
             ) {
                 Row(modifier = Modifier.padding(24.dp)) {
 
@@ -83,7 +91,6 @@ fun GenerateListOfAllOffersLoaded(offers: State<List<OfferResponse>>) {
                             fontWeight = FontWeight.Bold
                         )
                         Text(text = it.description)
-
                     }
                     Column() {
                         OutlinedButton(
