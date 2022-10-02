c160a3d581205231d39d1225f2e393ba0e140010
przemoai <again364@gmail.com>
2022-10-02 21:57:08 +0200

    no message

--- frontend_android/app/src/main/java/pl/pollub/harnasik/app/data/remote/HttpRoutes.kt
+++ frontend_android/app/src/main/java/pl/pollub/harnasik/app/data/remote/HttpRoutes.kt
@@ -3,7 +3,8 @@ package pl.pollub.harnasik.app.data.remote
 object HttpRoutes {
     //    THIS VALUE IS HARDCODED BECAUSE THIS WAY IT WORKS :)
     //    IT'S IPV4 ADDRES FROM YOUR PC NETWORK
-    private const val BASE_URL = "http://192.168.0.17:8080/api"
+    private const val IP_ADDRESS = "192.168.0.17"
+    private const val BASE_URL = "http://$IP_ADDRESS:8080/api"
     const val ALL_OFFERS = "$BASE_URL/offer"
     const val OFFER = "$BASE_URL/offer/"
 
