244860f33908dc747b0b62481f02a487474d32bd
szymon.marciniec <szymon.marciniec@pollub.edu.pl>
2022-10-19 14:24:43 +0200

    fixed text syntax

---
+++ frontend_android/app/src/main/java/pl/pollub/harnasik/app/presentation/login/Login.kt
@@ -0,0 +1,106 @@
+package pl.pollub.harnasik.app.presentation.login
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun LoginPage(navController: NavHostController) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        val username = remember { mutableStateOf(TextFieldValue()) }
+        val password = remember { mutableStateOf(TextFieldValue()) }
+
+        Text(
+            text = "Welcome back!",
+            style = TextStyle(fontSize = 50.sp, fontFamily = FontFamily.Cursive)
+        )
+        Spacer(modifier = Modifier.height(60.dp))
+        TextField(
+            label = { Text(text = "Username") },
+            value = username.value,
+            onValueChange = { username.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password") },
+            value = password.value,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            onValueChange = { password.value = it })
+
+        Spacer(modifier = Modifier.height(40.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = { },
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                LoginText()
+            }
+        }
+        OrText()
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = { },
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                SigninText()
+            }
+        }
+        Spacer(modifier = Modifier.height(60.dp))
+        ClickableText(
+            text = AnnotatedString("Forgot password?"),
+            onClick = { },
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Default
+            )
+        )
+    }
+}
+
+@Composable
+fun LoginText() {
+    Text("Log in", fontSize = 22.sp)
+}
+
+@Composable
+fun SigninText() {
+    Text("Sign up", fontSize = 22.sp)
+}
+
+@Composable
+fun OrText() {
+    Spacer(modifier = Modifier.height(10.dp))
+    Text(" OR ", fontSize = 16.sp)
+    Spacer(modifier = Modifier.height(10.dp))
+}
