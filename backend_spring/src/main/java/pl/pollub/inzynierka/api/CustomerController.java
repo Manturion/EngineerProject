fb8212c56a76d6b76c7f0a9555346609d263e61c
jakub <jakub.manturz123@gmail.com>
2022-11-02 21:33:01 +0100

    crated customer and get endpotins for him

---
+++ backend_spring/src/main/java/pl/pollub/inzynierka/api/CustomerController.java
@@ -0,0 +1,48 @@
+package pl.pollub.inzynierka.api;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.pollub.inzynierka.infrastructure.Customer.CreateCustomerDto;
+import pl.pollub.inzynierka.infrastructure.Customer.CustomerDto;
+import pl.pollub.inzynierka.infrastructure.Customer.CustomerService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequestMapping("/api/customer")
+@Controller
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Optional<CustomerDto> getCustomerId(@PathVariable Long id){
+        return customerService.getCustomerById(id);
+    }
+
+    @GetMapping()
+    @ResponseBody
+    public List<CustomerDto> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Long> createCustomer(@RequestBody CreateCustomerDto createCustomerDto){
+        return ResponseEntity.ok(customerService.createCustomer(createCustomerDto));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Long> deleteCustomer(@PathVariable Long id){
+        return ResponseEntity.ok(customerService.deleteCustomer(id));
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Optional<Long>> editCustomer(@RequestBody CustomerDto customerDto, @PathVariable Long id){
+        return ResponseEntity.ok(customerService.editCustomer(customerDto, id));
+    }
+}
